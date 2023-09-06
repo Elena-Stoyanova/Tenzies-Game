755dba32af2fa3c2601ad4317d075945380ea11c
Elena-Stoyanova <elenastoyanova742@gmail.com>
2023-09-06 18:47:37 +0300

    I update the Timer.js to store best time in localStorage

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,24 +1,45 @@
 import React from 'react'
 
-export default function Timer(props) {
+export default function Timer({ winTenzies, rollsCount, resetTimer }) {
+
     const [time, setTime] = React.useState(0)
-    const [isRunning, setIsRunning] = React.useState(true)
+    const [topScore, setTopScore] = React.useState(JSON.parse(localStorage.getItem('topScore')) || { rolls: null, time: 0 })
 
     React.useEffect(() => {
-        props.winTenzies ? setIsRunning(false) : setIsRunning(true)
+        if (winTenzies) {
+            if (topScore.rolls === null) {
+                console.log('yes')
+                setTopScore(prevBest => {
+                    return { ...prevBest, rolls: rollsCount, time: time }
+                })
+            }
 
-        if (props.resetTimer) {
+            if (rollsCount < topScore.rolls || time < topScore.time) {
+                setTopScore(prevBest => {
+                    return { ...prevBest, rolls: rollsCount, time: time }
+                })
+            }
+        }
+    }, [rollsCount, winTenzies, time, topScore.rolls, topScore.time])
+
+    React.useEffect(() => {
+        localStorage.setItem('topScore', JSON.stringify(topScore))
+    }, [topScore])
+
+    React.useEffect(() => {
+        if (resetTimer) {
             setTime(0)
         }
-    }, [props.resetTimer, props.winTenzies])
+    }, [resetTimer])
 
     React.useEffect(() => {
         let intervalId
-        if (isRunning) {
+        if (!winTenzies) {
             intervalId = setInterval(() => setTime(time + 1), 10)
         }
+
         return () => clearInterval(intervalId)
-    }, [isRunning, time])
+    }, [winTenzies, time])
 
     function formattedTime(time) {
         const minutes = Math.floor((time % 360000) / 6000).toString().padStart(2, "0")
@@ -28,7 +49,20 @@ export default function Timer(props) {
         return `${minutes} : ${seconds} : ${milliseconds}`
     }
 
+
     return (
-        <h3 className='time'>Time: {formattedTime(time)}</h3>
+        <>
+            <div className="time-container">
+                <h3 className='time'>Time: {formattedTime(time)}</h3>
+                <h3 className="rolls">Rolls: {rollsCount}</h3>
+            </div>
+            <fieldset className='topScore'>
+                <legend className='score-heading'>⭐ Top Score ⭐</legend>
+                <div className='score-stats'>
+                    <p className='top-time'>{topScore.time === 0 ? '00:00:00' : formattedTime(topScore.time)} 'ms</p>
+                    <p className='top-rolls'>{topScore.rolls === null ? '0' : topScore.rolls} rolls</p>
+                </div>
+            </fieldset>
+        </>
     )
 }
