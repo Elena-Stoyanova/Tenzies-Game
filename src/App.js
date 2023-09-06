0a6fb9076605be54ed5b9b2e6ca9dfc27cf69401
Elena-Stoyanova <elenastoyanova742@gmail.com>
2023-09-06 18:51:52 +0300

    I made some small changes to make the game look better.

--- src/App.js
+++ src/App.js
@@ -1,102 +1,38 @@
 import React from "react"
-import "./app.css"
-import "./die.css"
-import Die from "./Components/Die"
-import { nanoid } from "nanoid"
-import Confetti from "react-confetti"
-import Timer from "./Components/Timer"
-import logo from "./app-logo.png"
+import Game from "./Components/Game"
+import logo from "./App-logo.png"
+import Background from "./Background"
 
 export default function App() {
+  const [showGame, setShowGame] = React.useState(false)
+  const [city, setCity] = React.useState("")
 
-    const [dice, setDice] = React.useState(allNewDice())
-    const [winTenzies, setWinTenzies] = React.useState(false)
-    const [rollsCount, setRollsCount] = React.useState(0)
-    const [resetTimer, setResetTimer] = React.useState(false)
+  React.useEffect(() => {
+    fetch('https://extreme-ip-lookup.com/json/?key=9nLcPDX63sfyU3VJ0Sg9')
+      .then(res => res.json())
+      .then(response => {
+        setCity(response.city);
+      })
+      .catch((data, status) => {
+        console.log('Request failed:', data);
+      });
+  }, [])
 
-    React.useEffect(() => {
-        const allHeld = dice.every(die => die.isHeld)
-        const firstValue = dice[0].value
-        const allSameValue = dice.every(die => die.value === firstValue)
-        if (allHeld && allSameValue) {
-            setWinTenzies(true)
-        }
-    }, [dice])
+  function ClickHandler() {
+    setShowGame(true)
+  }
 
-    function generateNewDie() {
-        return {
-            value: Math.ceil(Math.random() * 6),
-            isHeld: false,
-            id: nanoid()
-        }
-    }
-
-    function allNewDice() {
-        const newDice = []
-        for (let i = 0; i < 10; i++) {
-            newDice.push(generateNewDie())
-        }
-        return newDice
-    }
-
-    function rollDice() {
-        //freeze dice
-        if (!winTenzies) {
-            setResetTimer(false)
-            setRollsCount(prevNum => prevNum + 1)
-            setDice(oldDice => oldDice.map(die => {
-                return die.isHeld ?
-                    die :
-                    generateNewDie()
-            }))
-        } else {
-            //new game
-            setRollsCount(0)
-            setWinTenzies(false)
-            setDice(allNewDice())
-            setResetTimer(true)
-        }
-    }
-
-    function holdDice(id) {
-        setDice(oldDice => oldDice.map(die => {
-            return die.id === id ?
-                { ...die, isHeld: !die.isHeld } :
-                die
-        }))
-    }
-
-    const diceElements = dice.map(die => (
-        <Die
-            key={die.id}
-            value={die.value}
-            isHeld={die.isHeld}
-            holdDice={() => holdDice(die.id)}
-        />
-    ))
-
-    return (
-        <main>
-            <img src={logo} className="app-logo" alt="logo" />
-            {winTenzies && <Confetti />}
-            <div className="dice-container">
-                {diceElements}
-            </div>
-
-            <div className="score-container">
-                <Timer
-                    winTenzies={winTenzies}
-                    resetTimer={resetTimer}
-                />
-                <h3 className="rolls">Rolls: {rollsCount}</h3>
-            </div>
-
-            <button
-                className="roll-dice--button"
-                onClick={rollDice}
-            >
-                {winTenzies ? "New Game" : "Roll"}
-            </button>
-        </main>
-    )
+  return (
+    <div>
+      {showGame ? <Game /> :
+        <div className="app">
+          <Background />
+          <img src={logo} className="logo" alt="logo" />
+          <h2 className="tittle">🎉  Hello user from {city}! 🎉</h2>
+          <p className="instructions">Roll until all dice are the same.Click each die to freeze it <br />
+            at its current value between rolls.</p>
+          <button className="app--button" onClick={ClickHandler}>Start Game</button>
+        </div>}
+    </div>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-import IntroPage from './IntroPage';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <IntroPage />
+    <App />
   </React.StrictMode>
 );
 
